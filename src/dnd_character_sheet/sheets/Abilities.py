ef9ac7193398a6259cc0146fabf6abab72c3277c
Alessandro Luciani <ale.is.lucio@gmail.com>
2018-04-02 23:44:12 +0200

    aggiornamento allineamento e razza del personaggio con test e aggiunto classi delle skills di ciascuna abilità

--- src/dnd_character_sheet/sheets/Abilities.py
+++ src/dnd_character_sheet/sheets/Abilities.py
@@ -29,3 +29,40 @@ class Abilities:
             return None
         else:
             return -5 + (int(value / 2))
+
+class AbilitySkillsStrength:
+    savingThrowsStrength = 0
+    athletics = 0
+
+class AbilitySkillsDexterity:
+    savingThrowsDexterity = 0
+    acrobatics = 0
+    sleight_of_hand = 0
+    stealth = 0
+
+class AbilitySkillsConstitution:
+    savingThrowsConstitution = 0
+
+class AbilitySkillsIntelligence:
+    savingThrowIntelligence = 0
+    arcana = 0
+    history = 0
+    investigation = 0
+    nature = 0
+    religion = 0
+
+class AbilitySkillsWisdom:
+    savingThrowsWisdom = 0
+    animalHandling = 0
+    insight = 0
+    medicine = 0
+    perception = 0
+    survival = 0
+
+class AbilitySkillsCharisma:
+    savingThrowCharisma = 0
+    deception = 0
+    intimidation = 0
+    performance = 0
+    persuasion = 0
+
--- tests/test_character.py
+++ tests/test_character.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from dnd_character_sheet.sheets.Character import Character, CharacterClass
+from dnd_character_sheet.sheets.Character import Character, CharacterClass , CharacterAlignment, CharacterRace
 
 __author__ = "Alessandro Luciani"
 __copyright__ = "Alessandro Luciani"
@@ -14,22 +14,23 @@ def test_new_character():
     assert character.getExperience() == 0, "A new character has 0 experience, aka adventurer in 'cass integration'"
     assert character.getLevel() == 1, "A new character starts from level 1"
     assert character.getClass() == CharacterClass.NO_CLASS, "A new character has no class"
-
+    assert character.getAlignment() == CharacterAlignment.NO_ALIGNMENT, " A new character has no alignment"
+    assert character.getRace() == CharacterRace.NO_RACE, " A new character has no race"
 
 def test_character_experience_gain():
     character = Character()
     character.setClass(CharacterClass.MONK)
     assert character.getClass() == CharacterClass.MONK
-    character.addExperience(100)
-    assert character.getLevel() == 2, "When the character reaches 100 experience, level increases to 2"
-    character.addExperience(399)
-    assert character.getLevel() == 2, "When the character reaches 499 experience, level is still 2"
+    character.addExperience(300)
+    assert character.getLevel() == 2, "When the character reaches 300 experience, level increases to 2"
+    character.addExperience(599)
+    assert character.getLevel() == 2, "When the character reaches 899 experience, level is still 2"
     character.addExperience(1)
-    assert character.getLevel() == 3, "When the character reaches 500 experience, level rises to 3"
+    assert character.getLevel() == 3, "When the character reaches 900 experience, level rises to 3"
 
 
 def test_unvalid_character_levels():
     character = Character()
-    character.addExperience(100000)
-    assert character.getLevel() is None, "A character can't go over 99999, over-experienced warriors can't find a job"
+    character.addExperience(380000)
+    assert character.getLevel() is None, "A character can't go over 355000, over-experienced warriors can't find a job"
 
