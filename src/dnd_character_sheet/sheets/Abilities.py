f7063fae4302a1c229c0c49212def06376d39fda
Alessandro Luciani <ale.is.lucio@gmail.com>
2018-04-06 23:25:36 +0200

    messo sotto la classe abiity , spero abbia un senso stavolta( ma non credo di essere cosi fortunato :-), ho avuto un po di difficolta a creare il test, sono andato un po a logica.

--- src/dnd_character_sheet/sheets/Abilities.py
+++ src/dnd_character_sheet/sheets/Abilities.py
@@ -34,51 +34,51 @@ class Abilities:
         else:
             return -5 + (int(value / 2))
 
-class AbilitySkillsStrength:
     savingThrowsStrength = 0
-    athletics = 0
 
     def trowSavingTrowsStrength (self):
         if self.savingThrowsStrength == 0:
-            return d20() + Abilities.getModifierStrength
+            return d20() + self.getModifierStrength()
         elif self.savingThrowsStrength == 1:
-            return d20() + Abilities.getModifierStrength + proficiencyBonus
+            return d20() + self.getModifierStrength + proficiencyBonus
+
+    athletics = 0
 
     def trowAthletics (self):
         if self.athletics == 0:
-            return d20() + Abilities.getModifierStrength
+            return d20() + self.getModifierStrength
         elif self.athletics == 1:
-            return d20() + Abilities.getModifierStrength
+            return d20() + self.getModifierStrength + proficiencyBonus
 
-class AbilitySkillsDexterity:
-    savingThrowsDexterity = 0
-    acrobatics = 0
-    sleight_of_hand = 0
-    stealth = 0
+#class AbilitySkillsDexterity:
+#    savingThrowsDexterity = 0
+#    acrobatics = 0
+#    sleight_of_hand = 0
+#    stealth = 0
 
-class AbilitySkillsConstitution:
-    savingThrowsConstitution = 0
+#class AbilitySkillsConstitution:
+#    savingThrowsConstitution = 0
 
-class AbilitySkillsIntelligence:
-    savingThrowIntelligence = 0
-    arcana = 0
-    history = 0
-    investigation = 0
-    nature = 0
-    religion = 0
+#class AbilitySkillsIntelligence:
+#    savingThrowIntelligence = 0
+#    arcana = 0
+#    history = 0
+#    investigation = 0
+#    nature = 0
+#    religion = 0
 
-class AbilitySkillsWisdom:
-    savingThrowsWisdom = 0
-    animalHandling = 0
-    insight = 0
-    medicine = 0
-    perception = 0
-    survival = 0
+#class AbilitySkillsWisdom:
+#    savingThrowsWisdom = 0
+#    animalHandling = 0
+#    insight = 0
+#    medicine = 0
+#    perception = 0
+#    survival = 0
 
-class AbilitySkillsCharisma:
-    savingThrowCharisma = 0
-    deception = 0
-    intimidation = 0
-    performance = 0
-    persuasion = 0
+#class AbilitySkillsCharisma:
+#    savingThrowCharisma = 0
+#    deception = 0
+#    intimidation = 0
+#    performance = 0
+#    persuasion = 0
 
